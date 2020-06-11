5baffcd4f9e3edf400cc8cbe3317581dca5a3ae0
Nana <annnojou@icloud.com>
2020-06-11 14:30:53 +0900

    initial

---
+++ src/mystylize.py
@@ -0,0 +1,85 @@
+from neural_style.stylize import stylize
+from PIL import Image
+import scipy
+import numpy as np
+import json
+
+ITERATIONS = 1000
+
+
+def neural_style_transfer(filename):
+
+    # default arguments
+    CONTENT_WEIGHT = 5e0
+    CONTENT_WEIGHT_BLEND = 1
+    STYLE_WEIGHT = 5e2
+    TV_WEIGHT = 1e2
+    STYLE_LAYER_WEIGHT_EXP = 1
+    LEARNING_RATE = 1e1
+    BETA1 = 0.9
+    BETA2 = 0.999
+    EPSILON = 1e-08
+    STYLE_SCALE = 1.0
+    VGG_PATH = "./neural_style/imagenet-vgg-verydeep-19.mat"
+    POOLING = "max"
+
+
+    CONTENT_PATH = f"./uploads/{filename}"
+    STYLE_PATH = "./neural_style/examples/hosidukiya.jpg"
+    OUTPUT_PATH = "./output/"
+
+
+    def imread(path):
+        im = Image.open(path)
+        im = im.convert("RGB")
+        im.thumbnail((500,500))
+        # img = scipy.misc.imread(path).astype(np.float)
+        # if len(img.shape) == 2:
+        #     # grayscale
+        #     img = np.dstack((img, img, img))
+        # elif img.shape[2] == 4:
+        #     # PNG with alpha channel
+        #     img = img[:, :, :3]
+        img = np.array(im)
+        print(img.shape)
+        return img
+
+
+    def imsave(path, img):
+        img = np.clip(img, 0, 255).astype(np.uint8)
+        Image.fromarray(img).save(path, quality=95)
+
+
+    for iteration, image, loss_vals in stylize(
+        network=VGG_PATH,
+        initial=None,
+        initial_noiseblend=1.0,
+        content=imread(CONTENT_PATH),
+        styles=[imread(STYLE_PATH)],
+        preserve_colors=False,
+        iterations=ITERATIONS,
+        content_weight=CONTENT_WEIGHT,
+        content_weight_blend=CONTENT_WEIGHT_BLEND,
+        style_weight=STYLE_WEIGHT,
+        style_layer_weight_exp=STYLE_LAYER_WEIGHT_EXP,
+        style_blend_weights=[1.0 / len([imread(STYLE_PATH)]) for _ in [imread(STYLE_PATH)]],
+        tv_weight=TV_WEIGHT,
+        learning_rate=LEARNING_RATE,
+        beta1=BETA1,
+        beta2=BETA2,
+        epsilon=EPSILON,
+        pooling=POOLING,
+        print_iterations=20,
+        checkpoint_iterations=20,
+    ):
+        if image is not None:
+            imsave(OUTPUT_PATH + f"{filename}.{iteration}.jpg", image)
+        if loss_vals is not None:
+            # print("loss",loss_vals)
+            #with open(OUTPUT_PATH+f"{filename}.{iteration}.json","w") as f:
+            #    f.write(json.dumps(loss_vals))
+            pass
+            
+
+    # imsave(OUTPUT_PATH + "output.jpg", image)
+
---
+++ src/web.py
@@ -0,0 +1,122 @@
+import os
+# request フォームから送信した情報を扱うためのモジュール
+# redirect  ページの移動
+# url_for アドレス遷移
+from flask import Flask, request, redirect, url_for,render_template, flash
+# ファイル名をチェックする関数
+from werkzeug.utils import secure_filename
+# 画像のダウンロード
+from flask import send_from_directory
+from mystylize import neural_style_transfer, ITERATIONS
+from threading import Thread
+from pathlib import Path
+
+UPLOAD_FOLDER = './uploads'
+# アップロードされる拡張子の制限
+ALLOWED_EXTENSIONS = set(['png', 'jpg',"jpeg"])
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["SECRET_KEY"] = "nanaaisiteru"
+
+def allwed_file(filename):
+    # .があるかどうかのチェックと、拡張子の確認
+    # OKなら１、だめなら0
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    # return '.' in filename and filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/',methods=["GET","POST"])
+def index():
+    if request.method == 'POST':
+        print(f"POST {request.files['file'].filename}")
+        # ファイルがなかった場合の処理
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        # データの取り出し
+        file = request.files['file']
+        # ファイル名がなかった時の処理
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        # ファイルのチェック
+        if file and allwed_file(file.filename):
+            # 危険な文字を削除（サニタイズ処理）
+            filename = secure_filename(file.filename)
+            # ファイルの保存
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            # アップロード後のページに転送
+            return redirect(url_for('status', filename=filename))
+    else:
+        return render_template('form.html.jinja', title='flask test') #変更
+
+
+is_traning = False
+
+def train(filename):
+    global is_traning
+    if not is_traning:
+        # Thread(target=neural_style_transfer,args=(filename,)).start()
+        is_traning = True
+        print("training start")
+        neural_style_transfer(filename)
+        is_traning = False
+
+@app.route('/status/<filename>')
+def status(filename):
+    numbers = []
+
+    for f in Path("./output").iterdir():
+        name_list = f.name.rsplit(".",2)
+        if name_list[0] == filename:
+            num = int(name_list[1])
+            numbers.append(num)
+
+    if len(numbers) == 0:
+        max_num = 0
+    else:
+        max_num = max(numbers)
+
+    return render_template("status.html.jinja",filename=filename, max_iteration=max_num)
+
+@app.route('/uploads/<filename>')
+@app.route('/uploads/<filename>/<iteration>')
+# ファイルを表示する
+def uploaded_file(filename,iteration):
+    numbers = []
+
+    for f in Path("./output").iterdir():
+        name_list = f.name.rsplit(".",2)
+        if name_list[0] == filename:
+            num = int(name_list[1])
+            numbers.append(num)
+
+    if len(numbers) == 0:
+        Thread(target=train,args=(filename,)).start()
+
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+    if iteration:
+        return send_from_directory("output", f"{filename}.{iteration}.jpg")
+
+    max_num = max(numbers)
+
+    if max_num < ITERATIONS-1:
+        Thread(target=train,args=(filename,)).start()
+
+    output_filename = f"{filename}.{max_num}.jpg"
+
+    return send_from_directory("output", output_filename)
+
+@app.route('/example/<filename>')
+# ファイルを表示する
+def example(filename):
+    return send_from_directory("neural_style/examples", filename)
+
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True,port=1234,host="0.0.0.0")
